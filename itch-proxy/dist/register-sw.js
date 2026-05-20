39e226586e0001637a1a4b2e604bb75c11b15ac6
fiqohixi <41038893-fiqohixi@users.noreply.replit.com>
2026-05-20 17:02:12 +0000

    Initial commit

---
+++ itch-proxy/dist/register-sw.js
@@ -0,0 +1,32 @@
+"use strict";
+const stockSW = "./sw.js";
+
+/**
+ * List of hostnames that are allowed to run serviceworkers on http://
+ */
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
+
+/**
+ * Global util
+ * Used in 404.html and index.html
+ */
+async function registerSW() {
+	let existed = false;
+	if ((await navigator.serviceWorker.getRegistration()) !== undefined) {
+		existed = true;
+		(await navigator.serviceWorker.getRegistration()).unregister();
+	}
+
+	if (!navigator.serviceWorker) {
+		if (
+			location.protocol !== "https:" &&
+			!swAllowedHostnames.includes(location.hostname)
+		)
+			throw new Error("Service workers cannot be registered without https.");
+
+		throw new Error("Your browser doesn't support service workers.");
+	}
+
+	await navigator.serviceWorker.register(stockSW);
+	return existed;
+}
---
+++ itch-proxy/start.sh
@@ -0,0 +1,5 @@
+caddy start --config ./Caddyfile
+/usr/local/cargo/bin/epoxy-server epoxy.toml
+
+# chmod +x ./woeful-x86_64-linux-musl
+# ./woeful-x86_64-linux-musl --config ./woeful.xml
